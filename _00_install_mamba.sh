85496d99b0e48c5a6de300f2b79e9234981cba36
TinasheMTapera <tinashemtapera@gmail.com>
2024-05-26 02:01:07 -0400

    test using HOME configuration

--- _00_install_mamba.sh
+++ _00_install_mamba.sh
@@ -16,7 +16,7 @@ function usage() {
 }
 
 # ensure path is given
-if [[ -n "$1" ]]; then
+if [[ -z "$1" ]]; then
     if [[ "${1?}" == -* ]] || [[ ! -d $1 ]]; then
         echo "Error: Invalid first argument"
         usage
